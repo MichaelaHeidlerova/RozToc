f710bf59116980549573356dafb628d695a63dd0
Gabriela Urbanová <gabul1990@gmail.com>
2025-06-07 11:09:31 +0200

    zobrazeni aktualniho casu

--- my-app/src/components/Main.jsx
+++ my-app/src/components/Main.jsx
@@ -2,19 +2,26 @@ import { Menu } from './Menu';
 import { Content } from './Content';
 
 export const Main = () => {
+  const today = new Date().toLocaleDateString('cs-CZ', {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
   return (
     <div className="layout-wrapper">
       <div className="teachersDesk">
         <div className="date-display">
           <h2>Dnes je:</h2>
+          <p>{today}</p>
         </div>
         <div className="desk2"></div>
       </div>
 
       <div className="main-container">
-        <Menu></Menu>
-
-        <Content></Content>
+        <Menu />
+        <Content />
       </div>
     </div>
   );
