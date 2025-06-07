3342db8ca1f79091469832f92994a943e696ed0b
Gabriela Urbanová <gabul1990@gmail.com>
2025-06-07 11:01:36 +0200

    instruction komponent

--- my-app/src/components/Content.jsx
+++ my-app/src/components/Content.jsx
@@ -1,11 +1,13 @@
 import { AboutUs } from "./pages/AboutUs";
+import { Instruction } from "./pages/Instruction";
 import { Intro } from "./pages/Intro";
 
 export const Content = () => {
   return (
     <div className="board">
       {/* <AboutUs></AboutUs> */}
-      <Intro></Intro>
+      {/* <Intro></Intro> */}
+      <Instruction></Instruction>
     </div>
   );
 };
