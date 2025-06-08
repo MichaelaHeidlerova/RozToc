28de1acc27d2cfcf2455bed75a0ad9dc579ccd5c
Michaela Heidlerová <michaela.heidlerova@gmail.com>
2025-06-08 10:34:03 +0200

    Načtení druhé podminky - nepřítonost

--- my-app/src/pages/Groups/VytvoreniSkupin.jsx
+++ my-app/src/pages/Groups/VytvoreniSkupin.jsx
@@ -1,20 +1,22 @@
-
 export const VytvoreniSkupin = ({ setStep, aktivniTridaHandler }) => {
   const volaniSkupin = JSON.parse(localStorage.getItem('seznamTrid')) || [];
 
   return (
-      <div>
-        <div className="pageTitle">
-          <h2>VYBER TŘÍDU</h2>
-        </div>
-        <div className="numberOfGroup">
+    <div>
+      <div className="pageTitle">
+        <h2>VYBER TŘÍDU</h2>
+      </div>
+      <div className="numberOfGroup">
         {volaniSkupin?.map((row) => (
-        <button key={row.nazevTridy} className="number" onClick={() => aktivniTridaHandler(row.nazevTridy)}>
-          {row.nazevTridy}
-        </button>
-      ))}
+          <button
+            key={row.nazevTridy}
+            className="number"
+            onClick={() => aktivniTridaHandler(row.nazevTridy)}
+          >
+            {row.nazevTridy}
+          </button>
+        ))}
       </div>
-        <button onClick={() => setStep("nepritomnost")}>Pokračovat na nepřítomnost</button>
-      </div>
-    );
+    </div>
+  );
 };
